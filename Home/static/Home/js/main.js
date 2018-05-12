1a01caeccabb0883c60515c9f45a60765dc2ed9a
Tejas jc <tejasjc@gmail.com>
2018-05-12 18:26:59 +0530

    js injection avoided

--- Home/static/Home/js/main.js
+++ Home/static/Home/js/main.js
@@ -142,7 +142,11 @@ typewriter.typeString('We don\'t just build Websites' )
     .start();
 
 
-
+function alpha(e) {
+    var k;
+    document.all ? k = e.keyCode : k = e.which;
+    return ((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8 || k == 32 || (k >= 48 && k <= 57));
+}
 
 
 
